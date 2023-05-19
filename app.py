31f89fab61c7bd8ab542dd9eb7bb72bec0ca3307
camenduru <54370274+camenduru@users.noreply.github.com>
2023-05-20 02:12:25 +0300

    test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-os.system(f"git config --global --add safe.directory '*'")
+# os.system(f"git config --global --add safe.directory '*'")
 os.system(f"git clone -b v2.2 https://github.com/camenduru/stable-diffusion-webui /home/demo/source/stable-diffusion-webui")
 os.chdir(f"/home/demo/source/stable-diffusion-webui")
 os.system(f"wget -q https://huggingface.co/ckpt/anything-v4.5-vae-swapped/resolve/main/anything-v4.5-vae-swapped.safetensors -O /home/demo/source/stable-diffusion-webui/models/Stable-diffusion/anything-v4.5-vae-swapped.safetensors")
