266e1ff00e5e7bc9537ec8004ded8c7dd208dfc3
W Roque <kulalanti@gmail.com>
2023-05-24 01:52:03 -0400

    reversed first change

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 os.system(f"git lfs install")
-os.system(f"git clone -b v2.2 https://github.com/kulalanti/stable-diffusion-webui-lambda")
+os.system(f"git clone -b v2.2 https://github.com/camenduru/stable-diffusion-webui /home/demo/source/stable-diffusion-webui")
 os.chdir(f"/home/demo/source/stable-diffusion-webui")
 
 os.system(f"git clone https://huggingface.co/embed/negative /home/demo/source/stable-diffusion-webui/embeddings/negative")
