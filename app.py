e2121ce6998fb95d1612933c9b860406ccc4c506
W Roque <kulalanti@gmail.com>
2023-05-24 01:43:48 -0400

    updated to reference fork repo
    updated to reference fork repo

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 os.system(f"git lfs install")
-os.system(f"git clone -b v2.2 https://github.com/camenduru/stable-diffusion-webui /home/demo/source/stable-diffusion-webui")
+os.system(f"git clone -b v2.2 https://github.com/kulalanti/stable-diffusion-webui-lambda")
 os.chdir(f"/home/demo/source/stable-diffusion-webui")
 
 os.system(f"git clone https://huggingface.co/embed/negative /home/demo/source/stable-diffusion-webui/embeddings/negative")
@@ -69,7 +69,6 @@ os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggin
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_sketch_sd15v2.pth -d /home/demo/source/stable-diffusion-webui/extensions/sd-webui-controlnet/models -o t2iadapter_sketch_sd15v2.pth")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/ControlNet-v1-1/resolve/main/t2iadapter_zoedepth_sd15v1.pth -d /home/demo/source/stable-diffusion-webui/extensions/sd-webui-controlnet/models -o t2iadapter_zoedepth_sd15v1.pth")
 
-#os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/prompthero/openjourney-v4/blob/main/openjourney-v4.ckpt -d /home/demo/source/stable-diffusion-webui/models/Stable-diffusion -o openjourney-v4")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/stabilityai/stable-diffusion-2-1/blob/main/v2-1_768-ema-pruned.safetensors -d /home/demo/source/stable-diffusion-webui/models/Stable-diffusion -o v2-1_768-ema-pruned.safetensors")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/dark-sushi-25d/resolve/main/darkSushi25D25D_v20.safetensors -d /home/demo/source/stable-diffusion-webui/models/Stable-diffusion -o darkSushi25D25D_v20.safetensors")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.ckpt -d /home/demo/source/stable-diffusion-webui/models/Stable-diffusion -o darkSushi25D25D_v20.vae.pt")
